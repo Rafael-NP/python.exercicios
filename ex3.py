e5ca225090152ed07059cbec5b300e398003ba54
Rafael <novaesrafael880@gmail.com>
2026-03-23 15:42:00 -0300

    03- exercício três

---
+++ ex3.py
@@ -0,0 +1,7 @@
+idVend = int(input("Digite o ID do vendedor: "))
+codPeca = int(input("Digite o código da peça: "))
+precoUni = float(input("Digite o preço unitário: "))
+qtd = int(input("Digite a quantidade vendida: "))
+print("ID do vendedor:", idVend)
+print("Código da peça:", codPeca)
+print("Comissão:", qtd * precoUni * 0.05)
